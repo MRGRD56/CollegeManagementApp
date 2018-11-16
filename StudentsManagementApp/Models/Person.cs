df2375149f614a7744dadff226d693158d7c31d2
Bukinistov Bukinist <Bukinist85@gmail.com>
2018-11-16 17:35:25 +0300

    v1.31.1 - Compilation errors - INotifyPropertyChanged realisation

--- StudentsManagementApp/Models/Person.cs
+++ StudentsManagementApp/Models/Person.cs
@@ -1,16 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using StudentsManagementApp.Pages.Students;
 
 namespace StudentsManagementApp.Models
 {
     public class Person
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public int Age { get; set; }
+
+        protected string lastName { get; set; }
+        protected string firstName { get; set; }
+        protected int age { get; set; }
+
+        public string LastName
+        {
+            get => lastName;
+            set
+            {   
+                lastName = value;
+                StudentsMgmtPageViewModel.OnPropertyChanged("LastName");
+            }
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                firstName = value;
+                OnPropertyChanged("FirstName");
+            }
+        }
+
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if (value < 14) throw new ArgumentException("Указан недопустимый возраст.");
+
+                age = value;
+                OnPropertyChanged("Age");
+            }
+        }
     }
 }
--- StudentsManagementApp/Models/Student.cs
+++ StudentsManagementApp/Models/Student.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace StudentsManagementApp.Models
 {
     [Table("Student")]
-    public class Student : Person
+    public class Student : Person, INotifyPropertyChanged
     {
         public int Id { get; set; }
         public virtual Group Group { get; set; }
+
+        //public event PropertyChangedEventHandler PropertyChanged;
+        //public void OnPropertyChanged([CallerMemberName]string prop = "") =>
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
     }
 }
